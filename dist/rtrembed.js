056910787bd5e65cf3f28af592936b8f69201a54
raymond-realtimemarketing <84166539+raymond-realtimemarketing@users.noreply.github.com>
2021-05-14 11:54:17 +0800

    dist rtrembed.js

---
+++ dist/rtrembed.js
@@ -0,0 +1,48 @@
+var globalbID_;
+var RTRLIB = RTRLIB || (function(){
+    var _args = {}; // private
+
+    return {
+        init : function(Args) {
+            _args = Args;
+            var bID = _args[0];
+            globalbID_=bID;
+            /*THIS IS FOR HTTP REQUEST*/
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var div = document.getElementById('rtr-embed-holder');
+                    div.innerHTML +=  this.responseText;
+                }
+            };
+            var params = 'bID='+bID;
+            var url = 'https://dashboard.realtimemarketing.com/embed_rtr?'+params;
+            console.log(url);
+            xhttp.open("GET", url, true);
+            xhttp.send();
+        },
+        closeEmbed: function(){
+            //put closing embed code here
+            rtrTogglePane();
+        },
+    };
+}());
+
+function rtrTogglePane() {
+    document.getElementById("rtr-widget").classList.toggle("visible");
+    document.getElementById("rtr-button--close").classList.toggle("visible");
+    document.getElementById("rtr-mask").classList.toggle("visible");
+    document.body.classList.toggle("rtr-is-active");
+
+    const element = document.querySelector("#rtr-button--close");
+    const visible = element.classList.contains("visible");
+    if(visible == true){
+        fetch('https://dashboard.realtimemarketing.com/p/rtr/actions?action=increment-page-views&bID='+globalbID_,
+            {
+                method: "POST",
+            },
+        ).then(function(response) {
+            console.log('rtr.pageviews++');
+        });
+    }
+}
